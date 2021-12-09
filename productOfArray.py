6523a9fa41a667f448b333255032ea71813e886c
v.adhi <adhi.pragantha@inlab.sequislife.com>
2021-12-09 08:45:07 +0700

    product of sum

---
+++ productOfArray.py
@@ -0,0 +1,24 @@
+# Parameters:
+#  arr: List[int]
+# return type: List[int]
+
+def productExceptSelf(arr):
+    lengthN = len(arr)
+    leftSum = [0] * lengthN
+    rightSum = [0] * lengthN
+
+    # right sum first
+    rightSum[lengthN-1] = 1
+    for i in range(lengthN-2,-1,-1):
+        rightSum[i] = rightSum[i+1] * arr[i+1]
+
+    leftSum[0] =1 
+
+    for i in range(1, lengthN):
+        leftSum[i] = leftSum[i-1] * arr[i-1]
+    
+    total = [ leftSum[i] * rightSum[i] for i in range(0,lengthN)]
+
+    return total
+
+print(productExceptSelf([2,5,3,4]))
