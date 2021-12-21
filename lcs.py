6733d1b3ecbb3bc9c932c833984b5899f15b3be2
inu <vr1090@gmail.com>
2021-12-21 20:05:12 +0700

    add dp solution

--- lcs.py
+++ lcs.py
@@ -14,9 +14,29 @@ def lcs(str1, str2,i=0,j=0, memoization={}):
     
     return memoization[key]
 
+
+def lcs2(str1, str2):
+    n = len(str1)
+    m = len(str2)
+
+    dp = [ [0] * (m+1) for i in range(n+1)]
+
+    for i in range(1, n+1):
+        for j in range(1, m+1):
+            if str1[i-1] == str2[j-1]:
+                dp[i][j] = 1 + dp[i-1][j-1]
+            else:
+                dp[i][j] = max( dp[i-1][j], dp[i][j-1])
+    
+    return dp[n][m]
+
+
 def main():
     assert(4 == lcs("abcd","abcd"))
     assert(2 == lcs("abgcdadga", "cdff"))
+    assert(4 == lcs2("abcd","abcd"))
+    assert(2 == lcs2("abgcdadga", "cdff"))
+
 
 if __name__=="__main__":
     main()
