d7aa4a81695bb322ab92addeaaa475a4dd27320f
inu <vr1090@gmail.com>
2021-12-22 22:46:42 +0700

    change ide

--- lowetCommonAncestor.py
+++ lowetCommonAncestor.py
@@ -1,9 +1,10 @@
 class Tree:
-    def __init__(self, data, left = None, right = None):
+    def __init__(self, data, left=None, right=None):
         self.data = data
         self.left = left
         self.right = right
-        
+
+
 # Parameters:
 #  root: Tree
 #  num1: int
@@ -16,12 +17,14 @@ def lowestCommonAncestor(root, num1, num2):
         return None
     if root.data == num1 or root.data == num2:
         return root
+
     leftLca = lowestCommonAncestor(root.left, num1, num2)
     rightLca = lowestCommonAncestor(root.right, num1, num2)
 
+
     if leftLca is not None and rightLca is not None:
         return root
-    
+
     if leftLca is not None:
         return leftLca
     else:
