8007366ecaceb7208b0ccf1069063d1f6e9137dc
inu <vr1090@gmail.com>
2021-12-25 00:57:58 +0700

    update interval solution

--- mergeInterval.py
+++ mergeInterval.py
@@ -2,7 +2,7 @@ def mergeIntervals(intervals):
     intervals.sort(key= lambda x:x[0])
 
     for i in range(len(intervals)-1):
-        if intervals[i][1] > intervals[i+1][0]:
+        if intervals[i][1] >= intervals[i+1][0]:
             intervals[i+1][0] = intervals[i][0]
             intervals[i+1][1] = max(intervals[i][1], intervals[i+1][1])
             intervals[i]= []
