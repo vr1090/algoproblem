72e8072e51e3a42eb5f0f74c772430d74fdb71b2
inu <vr1090@gmail.com>
2021-12-26 21:34:02 +0700

    add coin change

--- 46_coinChange.py
+++ 46_coinChange.py
@@ -6,17 +6,21 @@
 def coinChange(amount, coins,memoixe={}):
     # base case dulu
     key = amount
-
+    
     if memoixe.get(key) is not None:
         return memoixe[key]
 
+    logging("need to compute...", amount, memoixe)
     if amount <=0:
         return 0
+
+    if amount < min(coins):
+        return float("inf") 
     else:
-        results = float("inf")
+        results = 2000
         for c in coins:
             if ( amount - c) >=0:
-               tryCoin = 1 + coinChange(amount - c, coins)
+               tryCoin = 1 + coinChange(amount - c, coins, memoixe)
                results = min( results, tryCoin)
 
         memoixe[key] = results        
@@ -26,13 +30,13 @@ def coinChange(amount, coins,memoixe={}):
 logged = True
 def logging(*logs):
     if(logged):
-        print("setan", logs)
+        print("setan", *logs)
 
 def main():
     memoize = {}
-    result = coinChange(64, {1,5}, memoize)
-    logging(memoize)
-    assert(result == 16)
+    result = coinChange(15, {3,2,7}, memoize)
+    logging("result", result, memoize)
+    assert(result == 4)
 
 if __name__=="__main__":
     main()
